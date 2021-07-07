746d9fd2f5af3a75e40a6b6fcdd11a831f953915
Josiah <josiah.james.fowler@gmail.com>
2021-07-07 11:34:57 -0400

    created pages, routes, links, and added styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import React from "react";
 import Home from './pages/Home'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
-      <h1>Hi</h1>
-      <Home />
-
-      {/* Home (the one with the search on) */}
-
-      {/* SearchPage (the results page) */}
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
 
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import "./Home.css"
+import { Link } from "react-router-dom"
+import AppsIcon from "material-ui/icons/Apps"
+import Avatar from 'material-ui/Avatar';
 
 const Home = () => {
     return (
         <div className="home">
             <div className="home__header">
                 <div className="home__headerLeft">
-                    {/* Link */}
-                    {/* Link */}
+                
+                    <Link to='about'>About</Link>
+                    <Link to='about'>Store</Link>
                 </div>
                 <div className="home__headerRight">
-                    {/* Link */}
-                    {/* Link */}
-                    {/* Icon */}
-                    {/* Avatar */}
+                    <Link to='about'>Gmail</Link>
+                    <Link to='about'>Images</Link>
+                    <AppsIcon />
+                    <Avatar />
                 </div>
             </div>
             <div className="home__body">
