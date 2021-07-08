9116f89dd0dbb8fb1737a3dcfe0d1986e510ebac
Josiah <josiah.james.fowler@gmail.com>
2021-07-08 12:39:01 -0400

    rendered term state on search page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from "react";
 import Home from './pages/Home'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/search">
-            <h1>This is the search page</h1>
+            <SearchPage />
           </Route>
           <Route path="/">
             <Home />
